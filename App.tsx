f071389a04c631a247f5fba4b89b5cc9e3615129
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-11 18:57:14 -0300

    first configs

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.big}>0</Text>
+
+      <View style={styles.inline}>
+        <Button title='Remover'></Button>
+        <Button title='Adicionar'></Button>
+      </View>
+      
     </View>
   );
 }
@@ -17,4 +22,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  big: {
+    fontSize: 40
+  },
+  inline: {
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
 });
